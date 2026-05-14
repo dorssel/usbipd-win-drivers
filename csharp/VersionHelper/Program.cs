0448b1038fc09b64496fbc01452e53697d06a317
Frans van Dorsselaer <17404029+dorssel@users.noreply.github.com>
2026-05-14 22:43:07 +0200

    Add CI build action

---
+++ csharp/VersionHelper/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+using VersionHelper;
+
+if (args.Length != 3)
+{
+    Console.Error.WriteLine("Usage: VersionHelper <filename> <description> <path>");
+    return 1;
+}
+
+var template = new VersionInfo
+{
+    Filename = args[0],
+    Description = args[1]
+};
+var fullPath = Path.GetFullPath(args[2]);
+_ = Directory.CreateDirectory(Directory.GetParent(fullPath)!.FullName);
+File.WriteAllText(fullPath, template.TransformText(), Encoding.UTF8);
+return 0;
